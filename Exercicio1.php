f85538cdcd75c47e57267ec437dc185becced4dd
betinhojs <90387763+betinhojs@users.noreply.github.com>
2021-09-16 16:33:22 -0300

    Add files via upload

--- Exercicio1.php
+++ Exercicio1.php
@@ -4,21 +4,25 @@
 	$idades = array(44, 31, 30, 27, 20);
 	$emails = array ("Adalberto@faeterj-rio.edu.br", "Agda@faeterj-rio.edu.br", "Alexandre@faeterj-rio.edu.br", "Amanda@faeterj-rio.edu.br", "Brenda@faeterj-rio.edu.br");
 	$medias = array ( 7.5, 5.6, 8.7, 9.9, 9.7);
+	$profissao = array ("Tecnico de Laboratório", "Engenheira", "Professor", "Arquiteta", "Médica");
+	$bairro = array ("Riachuelo", "Méier", "Benfica", "Rocha", "Tijuca");
 ?>
 
 <!DOCTYPE html>
 <html>
 	<body>
 		
-		<h1>Tabela</h1>
+		<h1>PRIMEIRO EXERCÍCIO</h1>
 		
 		<table border="1">
-			
-			<tr>
+			<caption style ="font-family:'Times New Roman';font-size:25px;font-weight:bold;background-color: blue;color: black">TABELA</caption>			
+			<tr style ="font-family:'Times New Roman';font-size:20px;background-color: RoyalBlue;font-weight:bold;color: black">
 				<th>Nome</th>
 				<th>E-mail</th>
 				<th>Idade</th>
 				<th>Média</th>
+				<th>Profissão</th>
+				<th>Bairro</th>
 			</tr>
 			
 			<?php
@@ -30,10 +34,13 @@
 					echo "<td>$emails[$x]</td>";
 					echo "<td>$idades[$x]</td>";
 					echo "<td>$medias[$x]</td>";
+					echo "<td>$profissao[$x]</td>";
+					echo "<td>$bairro[$x]</td>";
 					echo "</tr>";
 				}
 			?>	
 			
 		</table>		
 	</body>
-</html>
+</html>				
+
