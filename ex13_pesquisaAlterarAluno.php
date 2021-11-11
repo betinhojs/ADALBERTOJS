4f84c7768c6abbfcc62427a9b42de17fe72024b0
betinhojs <90387763+betinhojs@users.noreply.github.com>
2021-11-10 23:22:11 -0300

    Add files via upload

---
+++ ex13_pesquisaAlterarAluno.php
@@ -0,0 +1,19 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h1>Alterar Aluno</h1>
+<br>
+
+<br>
+<form action="ex13_alterarAluno.php" method=GET>
+    Digite a matricula para editar: <input type=text name="matricula_editar"> <br>
+
+    <input type="submit" value="Pesquisar e alterar">
+</form>
+<br>
+</body>
+</html>
